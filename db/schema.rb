f958398f2dc512ab67a35d3d224da0e6b2367737
diugalde <dua.0695@gmail.com>
2015-05-17 01:55:38 -0600

    Adding accepted and flagged columns to professor,course and comment

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150514060419) do
+ActiveRecord::Schema.define(version: 20150517075324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,8 +21,9 @@ ActiveRecord::Schema.define(version: 20150514060419) do
     t.integer  "user_id"
     t.integer  "commentable_id"
     t.string   "commentable_type"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+    t.boolean  "flagged",          default: false
   end
 
   add_index "comments", ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id", using: :btree
@@ -42,8 +43,9 @@ ActiveRecord::Schema.define(version: 20150514060419) do
     t.string   "name"
     t.float    "overall_difficulty"
     t.integer  "school_id"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
+    t.boolean  "accepted",           default: false
   end
 
   add_index "courses", ["school_id"], name: "index_courses_on_school_id", using: :btree
@@ -121,6 +123,7 @@ ActiveRecord::Schema.define(version: 20150514060419) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "accepted",               default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
