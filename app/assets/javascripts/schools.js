f40fb400f9788c6c06d67b68d7417551426606e5
Luis <luis-ex@live.com>
2015-05-17 18:37:52 +0000

    Starting pages for universities and schools
    - Images for university banners complete
    - Styles for university info. almost complete
    - Integrating isotope for schools view
    - Adding buttons for schools sorting

--- app/assets/javascripts/schools.js
+++ app/assets/javascripts/schools.js
@@ -1,2 +1,16 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+var $container = $('.isotope').isotope({
+    layoutMode: 'fitRows',
+    itemSelector: '.element-item',
+    getSortData: {
+      name: '.name',
+    }
+});
+
+// bind sort button click
+$('#sorts').on( 'click', 'button', function() {
+    var sortByValue = $(this).attr('data-sort-by');
+    $container.isotope({ sortBy: sortByValue });
+});
